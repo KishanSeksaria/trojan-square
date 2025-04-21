23f76e349b7a1cde2a37d485cc135fbae9d34500
Kishan Seksaria <kishanseksaria@gmail.com>
2025-04-20 21:13:29 -0700

    add vercel ai sdk with groq

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,13 +1,23 @@
+'use client'
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription
 } from '@/components/ui/card'
+import { useQuery } from 'convex/react'
 import React from 'react'
+import { api } from '../../../../convex/_generated/api'
+import { Id } from '../../../../convex/_generated/dataModel'
+import { useParams } from 'next/navigation'
 
-async function page({ params }: { params: Promise<{ chatId: string }> }) {
-  const { chatId } = await params
+function ChatPage() {
+  // Convert the string chatId to a Convex ID
+  const { chatId } = useParams()
+  const chatMessages = useQuery(api.messages.getAllByChatId, {
+    chatId: chatId as Id<'chats'>
+  })
+
   return (
     <div className='flex h-full w-full items-center justify-center'>
       <Card className='w-[400px]'>
@@ -20,4 +30,4 @@ async function page({ params }: { params: Promise<{ chatId: string }> }) {
   )
 }
 
-export default page
+export default ChatPage
