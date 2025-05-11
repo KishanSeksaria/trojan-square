294007a64e811bf094f3550e2512a9706a535a6c
Kishan Seksaria <kishanseksaria@gmail.com>
2025-05-11 15:02:46 -0700

    refactor: comment out unused configuration options in next.config.ts for clarity

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  outputFileTracingIncludes: {
-    'api/**': ['./local_models/all-MiniLM-L6-v2/**']
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      if (Array.isArray(config.externals)) {
-        config.externals = [
-          ...config.externals,
-          { 'onnxruntime-node': 'commonjs onnxruntime-node' }
-        ]
-      } else {
-        config.externals = ['onnxruntime-node']
-      }
-    }
-    return config
-  }
+  // output: 'standalone',
+  // outputFileTracingIncludes: {
+  //   'api/**': ['./local_models/all-MiniLM-L6-v2/**']
+  // },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     if (Array.isArray(config.externals)) {
+  //       config.externals = [
+  //         ...config.externals,
+  //         { 'onnxruntime-node': 'commonjs onnxruntime-node' }
+  //       ]
+  //     } else {
+  //       config.externals = ['onnxruntime-node']
+  //     }
+  //   }
+  //   return config
+  // }
 }
 
 export default nextConfig
